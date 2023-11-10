ae859d10f7b8b2401f6e4d5e79c8917d78061ee6
JellyBoonz <zitropiano@gmail.com>
2023-11-09 23:35:21 -0500

    Add unit test for CPU.add()

--- src/cpu.rs
+++ src/cpu.rs
@@ -1,3 +1,11 @@
+enum Instruction {
+    ADD(ArithmeticTarget),
+    }
+    
+enum ArithmeticTarget {
+    A, B, C, D, E, H, L,
+}
+
 struct Registers {
     a: u8,
     b: u8,
@@ -9,13 +17,24 @@ struct Registers {
     l: u8,
 }
 
-struct FlagsRegister {
-    zero: bool,
-    subtract: bool,
-    half_carry: bool,
-    carry: bool
-}
+struct CPU {
+    registers: Registers,
+    pc: u16,
+    bus: MemoryBus,
+  }
 
+  struct MemoryBus {
+    memory: [u8; 0xFFFF]
+  }
+  
+  impl MemoryBus {
+    fn read_byte(&self, address: u16) -> u8 {
+      self.memory[address as usize]
+    }
+  }
+
+// There are functions which allow the game to read/write 2 bytes at the same
+// time to combined register ("af", "bc", "de", "hl")
 impl Registers {
     fn get_bc(&self) -> u16 {
         (self.b as u16) << 8 | self.c as u16
@@ -54,6 +73,13 @@ impl Registers {
     }
 }
 
+struct FlagsRegister {
+    zero: bool,
+    subtract: bool,
+    half_carry: bool,
+    carry: bool
+}
+
 const ZERO_FLAG_BYTE_POSITION: u8 = 7;
 const SUBTRACT_FLAG_BYTE_POSITION: u8 = 6;
 const HALF_CARRY_FLAG_BYTE_POSITION: u8 = 5;
@@ -83,3 +109,99 @@ impl std::convert::From<u8> for FlagsRegister {
         }
     }
 }
+
+impl CPU {
+
+    pub fn new() -> CPU {
+        CPU {
+            registers: Registers {
+                a: 0,
+                b: 0,
+                c: 0,
+                d: 0,
+                e: 0,
+                f: 0,
+                h: 0,
+                l: 0,
+            },
+            pc: 0,
+            bus: MemoryBus { memory: [0; 0xFFFF] },
+        }
+    }
+    fn execute(&mut self, instruction: Instruction) {
+        match instruction {
+          Instruction::ADD(target) => {
+            match target {
+                ArithmeticTarget::A => {
+                    let value = self.registers.a;
+                    let new_value = self.add(value);
+                    self.registers.a = new_value;
+                }
+                ArithmeticTarget::B => {
+                    let value = self.registers.b;
+                    let new_value = self.add(value);
+                    self.registers.a = new_value;
+                }
+                ArithmeticTarget::C => {
+                    let value = self.registers.c;
+                    let new_value = self.add(value);
+                    self.registers.a = new_value;
+                }
+                ArithmeticTarget::D => {
+                    let value = self.registers.d;
+                    let new_value = self.add(value);
+                    self.registers.a = new_value;
+                }
+                ArithmeticTarget::E => {
+                    let value = self.registers.e;
+                    let new_value = self.add(value);
+                    self.registers.a = new_value;
+                }
+                ArithmeticTarget::H => {
+                    let value = self.registers.h;
+                    let new_value = self.add(value);
+                    self.registers.a = new_value;
+                }
+                ArithmeticTarget::L => {
+                    let value = self.registers.l;
+                    let new_value = self.add(value);
+                    self.registers.a = new_value;
+                }
+                _ => { /* TODO: support more targets */ }
+            }
+          }
+          _ => { /* TODO: support more instructions */ }
+        }
+      }
+    
+    fn add(&mut self, value: u8) -> u8 {
+        let (new_value, did_overflow) = self.registers.a.overflowing_add(value);
+        // TODO: set flags
+        // self.registers.f.zero = new_value == 0;
+        // self.registers.f.subtract = false;
+        // self.registers.f.carry = did_overflow;
+        // // Half Carry is set if adding the lower nibbles of the value and register A
+        // // together result in a value bigger than 0xF. If the result is larger than 0xF
+        // // than the addition caused a carry from the lower nibble to the upper nibble.
+        // self.registers.f.half_carry = (self.registers.a & 0xF) + (value & 0xF) > 0xF;
+        new_value
+        }
+    }
+
+    #[cfg(test)]
+    mod tests {
+        // Define your unit tests within this module
+        use super::*;
+        #[test]
+        fn test_add_instruction() {
+            let mut cpu = CPU::new();
+            cpu.registers.a = 0x10;
+            cpu.registers.b = 0x20;
+    
+            // Call the function that executes the ADD instruction
+            cpu.execute(Instruction::ADD(ArithmeticTarget::B));
+    
+            // Assert the expected results
+            assert_eq!(cpu.registers.a, 0x30);
+        }
+    }
